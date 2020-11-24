c4901c7973caa8f7962bc53f0a1a3e98df271ed5
Hugo Birch <hugo.birch@epfl.ch>
2020-11-24 16:44:07 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/Project_Monte_carlo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_Monte_carlo.dir/main.cpp.o"
+  "Project_Monte_carlo"
+  "Project_Monte_carlo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_Monte_carlo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Project_Monte_carlo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Project_Monte_carlo.
+# This may be replaced when dependencies are built.
