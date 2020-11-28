e9da2534e6b821ffbb4855c83e569068a83c1d0e
Hugo Birch <hugo.birch@epfl.ch>
2020-11-28 19:03:52 +0100

    Cmake for boost

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/mehdi/Documents/MA3/git_PCSP/Project_monte_carlo
+# Install script for directory: /home/hugo/MA3/Project_monte_carlo
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,7 +29,7 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "0")
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
 # Is this installation the result of a crosscompile?
@@ -39,7 +39,12 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/mehdi/Documents/MA3/git_PCSP/Project_monte_carlo/cmake-build-debug/Modules/cmake_install.cmake")
+  include("/home/hugo/MA3/Project_monte_carlo/cmake-build-debug/Test/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/hugo/MA3/Project_monte_carlo/cmake-build-debug/Modules/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +55,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/mehdi/Documents/MA3/git_PCSP/Project_monte_carlo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/hugo/MA3/Project_monte_carlo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
