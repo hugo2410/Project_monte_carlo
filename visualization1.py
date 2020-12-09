d96689d30e482425108f4ea3bcc22bfba13d00dd
Hugo Birch <hugo.birch@epfl.ch>
2020-12-09 21:56:54 +0100

    Runall gtest + cleaned program

--- visualization1.py
+++ visualization1.py
@@ -1,45 +1,51 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
 
 
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from scipy.stats import norm
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import os
 
 
-# In[13]:
 
 
+#path to the directory where to find the files
 directory = "output"
-fig = plt.subplots(nrows = 1, ncols = 1, sharex = False, sharey = False, figsize = (13, 10))
+
+#defines subplots settings
+fig,ax = plt.subplots(nrows = 1, ncols = 2, sharex = False, sharey = False, figsize = (19, 10))
 var1=[]
 var2=[]
 l=[]
+#Loops over all files in directory
 for root,dirs,files in os.walk(directory):
     for file in tqdm(files):
+
+        #only retains correct files
         if file.endswith(".csv") and file.startswith("sample"):
             file_path = os.path.join(directory,file)
             df= pd.read_csv(file_path)
             
-            sns.distplot(df.iloc[1:,:],label=file)
+            sns.distplot(df.iloc[1:,:], label=file,ax=ax[ 0])
             
             l.append((float)(file.split("_")[-1].split(".")[0]))
             var1.append(df.iloc[0,0]/(float)(file.split("_")[-1].split(".")[0]))
             var2.append(df.iloc[1:,0].var())
-
+ax[0].legend()
+ax[0].set_xlabel('Means')
+ax[0].set_ylabel('Count')
+ax[1].scatter(l,var1)
+ax[1].scatter(l,var2)
+ax[1].set_ylim(0,0.003)
 plt.legend()
 plt.show()
-plt.scatter(l,var1)
-plt.scatter(l,var2)
-plt.ylim(0,0.003)
+fig.savefig("CTLTheorem.png")
 
-
-# In[11]:
 
 
 df=pd.read_csv("cmake-build-debug/output/moments.csv", header=None)
@@ -48,7 +54,7 @@ df_separated.columns= df.iloc[0,0].split(";")
 #df_separated['Statistical Moment']=df.iloc[:,0].str.split(pat=";",expand=True)[1].astype(float)
 
 sns.barplot(x=df_separated['Order'], y =df_separated['Statistical Moment'])
-
+fig.savefig("Moment.png")
 
 
 
